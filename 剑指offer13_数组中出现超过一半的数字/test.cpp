f46659f7d54fcd9eedbf45e624d20e6adb4d3aa0
浅忆梦微凉 <3538922590@qq.com>
2019-01-19 16:34:14 +0800

    第二中方法通过

--- 剑指offer13_数组中出现超过一半的数字/test.cpp
+++ 剑指offer13_数组中出现超过一半的数字/test.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-class Solution {
+class Solution1 {
 public:
 	int MoreThanHalfNum_Solution(vector<int> numbers) {
 		//排序
@@ -30,16 +30,52 @@ public:
 	}
 };
 
+class Solution2 {
+public:
+	int MoreThanHalfNum_Solution(vector<int> numbers) {
+		int length = numbers.size();
+		if (length == 0)
+			return 0;
+		sort(numbers.begin(), numbers.end());
+		int max_num = 0;
+		int num = 0;
+		int val = numbers[0];
+		for (int i = 0; i < length; i++) {
+			if (val == numbers[i]) {
+				num++;
+			}
+			else {
+				max_num = num > max_num ? num : max_num;
+				if (max_num > length / 2)
+					return val;
+				val = numbers[i];
+				num = 1;
+			}
+		}
+		max_num = num > max_num ? num : max_num;
+		if (max_num > length / 2)
+			return val;
+		else
+			return 0;
+	}
+};
+
 void TestFunc1(){
 	vector<int> array = { 1, 2, 2, 3, 2, 2, 4 };
-	Solution s1;
+	Solution1 s1;
 	cout << "出现超过一半的数字为：";
 	cout<<s1.MoreThanHalfNum_Solution(array)<<endl;
+}
 
+void TestFunc2(){
+	vector<int> array = { 1, 2, 2, 3, 2, 2, 4 };
+	Solution2 s2;
+	cout << "出现超过一半的数字为：";
+	cout << s2.MoreThanHalfNum_Solution(array) << endl;
 }
 
 int main(){
-	TestFunc1();
+	TestFunc2();
 	system("pause");
 	return 0;
 }
